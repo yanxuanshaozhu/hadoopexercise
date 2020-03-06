64f2d2fdd497babff4c6e276a9f2b5c7322a90f3
yanxuanshaozhu <35414074+yanxuanshaozhu@users.noreply.github.com>
2020-03-06 21:09:17 +0800

    Add files via upload

---
+++ partitionwritablecomparabletest1/APartitioner.java
@@ -0,0 +1,23 @@
+package pers.yanxuanshaozhu.partitionwritablecomparabletest1;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class APartitioner extends Partitioner<ComparableFlowBean, Text> {
+
+    public int getPartition(ComparableFlowBean comparableFlowBean, Text text, int i) {
+        String phone = text.toString().substring(0, 3);
+
+        if ("136".equals(phone)) {
+            return 0;
+        } else if ("137".equals(phone)) {
+            return 1;
+        } else if ("138".equals(phone)) {
+            return 2;
+        } else  if ("139".equals(phone)) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+}
---
+++ writablecomparabletest1/ComparableFlowBean.java
@@ -0,0 +1,65 @@
+package pers.yanxuanshaozhu.writablecomparabletest1;
+
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class ComparableFlowBean implements WritableComparable<ComparableFlowBean> {
+
+    private long upFlow;
+    private long downFlow;
+    private long sumFlow;
+
+    public long getUpFlow() {
+        return upFlow;
+    }
+
+    public long getDownFlow() {
+        return downFlow;
+    }
+
+    public long getSumFlow() {
+        return sumFlow;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "FlowBean{" +
+                "upFlow=" + upFlow +
+                ", downFlow=" + downFlow +
+                ", sumFlow=" + sumFlow +
+                '}';
+    }
+
+    public void setUpFlow(long upFlow) {
+        this.upFlow = upFlow;
+    }
+
+    public void setDownFlow(long downFlow) {
+        this.downFlow = downFlow;
+    }
+
+    public void setSumFlow(long sumFlow) {
+        this.sumFlow = sumFlow;
+    }
+
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeLong(upFlow);
+        dataOutput.writeLong(downFlow);
+        dataOutput.writeLong(sumFlow);
+    }
+
+    public void readFields(DataInput dataInput) throws IOException {
+        this.upFlow = dataInput.readLong();
+        this.downFlow = dataInput.readLong();
+        this.sumFlow = dataInput.readLong();
+    }
+
+    public int compareTo(ComparableFlowBean o) {
+        return (int) (o.getSumFlow() - this.getSumFlow());
+    }
+}
