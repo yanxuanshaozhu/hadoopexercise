1cad6d52c58801cdcea52d1b4e383514a53766cb
yanxuanshaozhu <35414074+yanxuanshaozhu@users.noreply.github.com>
2020-03-07 19:37:13 +0800

    Add files via upload

---
+++ reducejointest1/OrderComparator.java
@@ -0,0 +1,16 @@
+package pers.yanxuanshaozhu.reducejointest1;
+
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+public class OrderComparator extends WritableComparator {
+
+    protected OrderComparator() {
+        super(OrderBean.class, true);
+    }
+
+    @Override
+    public int compare(WritableComparable a, WritableComparable b) {
+        return ((OrderBean) a).getPid().compareTo(((OrderBean) b).getPid());
+    }
+}
