f55c1ff1730c6add82f45a8891a8f5c2ab531165
yanxuanshaozhu <35414074+yanxuanshaozhu@users.noreply.github.com>
2020-03-07 13:10:36 +0800

    Add files via upload

---
+++ outputformatimitationtest1/LogReducer.java
@@ -0,0 +1,17 @@
+package pers.yanxuanshaozhu.outputformatimitationtest1;
+
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class LogReducer extends Reducer<LogBean, NullWritable, LogBean, NullWritable> {
+
+
+    @Override
+    protected void reduce(LogBean key, Iterable<NullWritable> values, Context context) throws IOException, InterruptedException {
+        for (NullWritable value : values) {
+            context.write(key,value);
+        }
+    }
+}
---
+++ outputformattest1/LogDriver.java
@@ -0,0 +1,28 @@
+package pers.yanxuanshaozhu.outputformattest1;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class LogDriver {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+
+        Job job = Job.getInstance(new Configuration(), "OutputFormat Job");
+
+        job.setJarByClass(LogDriver.class);
+
+        job.setOutputFormatClass(AOutputFormat.class);
+
+
+        FileInputFormat.setInputPaths(job, new Path("D:\\hadoopdata\\input"));
+        FileOutputFormat.setOutputPath(job, new Path("D:\\hadoopdata\\output"));
+
+        boolean b = job.waitForCompletion(true);
+        System.exit(b ? 0 : 1);
+    }
+}
